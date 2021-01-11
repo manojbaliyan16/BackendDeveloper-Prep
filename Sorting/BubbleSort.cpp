59c77853481c6bc6d78923ccef7d5aa9318541ec
Manoj <70482944+manojbaliyan16@users.noreply.github.com>
2021-01-12 00:18:13 +0530

    BubbleSort

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -1,1 +1,36 @@
 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<int> bubbleSort(std::vector<int> &vec)
+{
+	int counter=0;
+	bool isSorted=false;
+	while(!isSorted)
+	{
+		isSorted=true;
+		for(int i=0;i<vec.size()-1-counter;++i)
+		{
+			if(vec[i]>vec[i+1])
+			{
+				swap(vec[i],vec[i+1]);
+				isSorted=false;
+			}
+		}
+		counter+=1;
+	}
+	return vec;
+}
+
+int main()
+{
+
+    vector<int> result{8,5,2,9,5,6,3};
+    vector<int> res=bubbleSort(result);
+    for(auto const &elem:res)
+    cout << elem << " ";
+    cout << endl;
+    return 0;
+}
